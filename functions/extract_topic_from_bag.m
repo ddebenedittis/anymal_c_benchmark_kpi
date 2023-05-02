a1b98707d49720d9265dce08903c6462700b59ac
ddebenedittis <davide.debenedittis@gmail.com>
2023-05-02 17:03:49 +0200

    Can downsample the topic extraction

--- functions/extract_topic_from_bag.m
+++ functions/extract_topic_from_bag.m
@@ -1,9 +1,13 @@
-function msgStructs = extract_topic_from_bag(rosbag_file,topic_name)
+function msgStructs = extract_topic_from_bag(rosbag_file, topic_name, downsampling_factor)
 % EXTRACT TOPICS FROM BAGS 
 % Extracts the message structure from bag file
 
 bag = rosbag(rosbag_file);
 bSel = select(bag,'Topic',topic_name);
-msgStructs = readMessages(bSel,'DataFormat','struct');
+if nargin == 2
+    msgStructs = readMessages(bSel, 'DataFormat', 'struct');
+else
+    msgStructs = readMessages(bSel, 1:downsampling_factor:bSel.NumMessages, 'DataFormat', 'struct');
+end
 
 end
