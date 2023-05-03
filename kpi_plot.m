86d64a98deaf667448338d876a8e5cb4cfa6af54
ddebenedittis <davide.debenedittis@gmail.com>
2023-05-03 16:46:27 +0200

    New plot comparing KPIs vs trial number

--- kpi_plot.m
+++ kpi_plot.m
@@ -16,13 +16,31 @@ for i = 1:length(fn)
     [~, ~] = mkdir("plots/" + string(fni));
 end
 
+%% Correct the trials number
+
+experiments_data.Trial(1) = 1;
+
+trial = 1;
+
+for i = 2:size(experiments_data, 1)
+    if isequal(experiments_data(i, 2:8), experiments_data(i - 1, 2:8))
+        trial = trial + 1;
+    else
+        trial = 1;
+    end
+
+    experiments_data.Trial(i) = trial;
+end
+
 %% Save all the plots
 
-plotComparingEnvironments(experiments_data, "stepbystep", "trekker")
-plotComparingEnvironments(experiments_data, "4in1", "trekker")
-plotComparingEnvironments(experiments_data, "4in1", "dynamic_gaits")
+plotComparingEnvironments(experiments_data, "stepbystep", "trekker");
+plotComparingEnvironments(experiments_data, "4in1", "trekker");
+plotComparingEnvironments(experiments_data, "4in1", "dynamic_gaits");
 
-plotComparingControllers(experiments_data)
+plotComparingControllers(experiments_data);
+
+plot_cots_vs_trials(experiments_data);
 
 %% Elaborate data
 
@@ -286,7 +304,7 @@ function ax = plot_kpi(x_axs, kpi_mean, kpi_std, name_1, i_1, n_1, speeds, i_spe
     );
     title("Cost of Transport (Energy Usage)")
     xlabel("Inclination [deg]")
-    ylabel("CoT_1")
+    ylabel("CoT")
     legend('Location', 'Best')
 
     ax(2) = subplot(2,2,3);
@@ -327,6 +345,82 @@ function ax = plot_kpi(x_axs, kpi_mean, kpi_std, name_1, i_1, n_1, speeds, i_spe
     xlabel("Inclination [deg]")
     ylabel("Slippage")
     legend('Location', 'Best')
+end
+
+
+%% KPI values vs the trial number
+
+% Attempt to see if the KPI vary with the battery level (successive tests
+% have a lower battery charge).
+
+function plot_cots_vs_trials(experiments_data)
+    kpi_mean = repmat(experiments_data.kpi(1).full, 3, 1);
+    kpi_std = repmat(experiments_data.kpi(1).full, 3, 1);
+
+    fn = fieldnames(experiments_data.kpi(1).full);
+    n_fields = length(fn);
+
+    for trial = 1:3
+        idx = experiments_data.Trial == trial;
+
+        data = vertcat(experiments_data.kpi(idx).full);
+            
+        for i_field = 1:n_fields
+            kpi_mean(trial).(fn{i_field}) = mean(vertcat(data.(fn{i_field})));
+            kpi_std(trial).(fn{i_field}) = std(vertcat(data.(fn{i_field})));
+        end
+    end
+
+    fig = figure('Position', get(0, 'Screensize'), 'visible','off');
+    hold on
+
+    subplot(2,2,1)
+    hold on
+    errorbar(vertcat(kpi_mean(:).CoT), vertcat(kpi_std(:).CoT))
+    title("Cost of Transport (Energy Usage)")
+    xlabel("Trial")
+    ylabel("CoT")
+
+    subplot(2,2,2)
+    hold on
+    errorbar(vertcat(kpi_mean(:).CoT_1), vertcat(kpi_std(:).CoT_1))
+    title("Cost of Transport (Mechanical Energy)")
+    xlabel("Inclination [deg]")
+    ylabel("CoT_1")
+
+    subplot(2,2,3)
+    hold on
+    errorbar(vertcat(kpi_mean(:).Dev_y), vertcat(kpi_std(:).Dev_y))
+    title("Lateral Deviation")
+    xlabel("Inclination [deg]")
+    ylabel("Dev_y")
+
+    subplot(2,2,4)
+    hold on
+    errorbar(vertcat(kpi_mean(:).Slippage), vertcat(kpi_std(:).Slippage))
+    title("Slippage")
+    xlabel("Inclination [deg]")
+    ylabel("Slippage")
+
+    figure_title = "KPIs vs trial number";
+            
+    sgtitle(figure_title)
+
+    % set(fig, 'visible', 'on');
+    % set all units inside figure to normalized so that everything is scaling accordingly
+    set(findall(fig,'Units','pixels'),'Units','normalized');
+    % set figure units to pixels & adjust figure size
+    fig.Units = 'pixels';
+    fig.OuterPosition = [0 0 1366 768];
+    % define resolution figure to be saved in dpi
+    res = 420;
+    % recalculate figure size to be saved
+    set(fig,'PaperPositionMode','manual')
+    fig.PaperUnits = 'inches';
+    fig.PaperPosition = [0 0 1366 768]/res;
+    exportgraphics(fig, "plots/" + "/" + figure_title + ".pdf", 'ContentType', 'vector');
+
+    close(fig)
 end
 
 %%
