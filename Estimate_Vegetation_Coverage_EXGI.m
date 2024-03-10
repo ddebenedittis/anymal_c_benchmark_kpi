eda6d43e0e307fad1e197a9301d80cbc1ae4c19a
Davide De Benedittis <davide.debenedittis@gmail.com>
2024-03-10 23:33:32 +0100

    Added percentage + more colorful image

--- Estimate_Vegetation_Coverage_EXGI.m
+++ Estimate_Vegetation_Coverage_EXGI.m
@@ -10,7 +10,7 @@ clc;
 
 %% Params
 
-exgi_threshold = 12;
+exgi_threshold = 16;
 
 % Modified
 % min_green = 100;
@@ -47,6 +47,9 @@ if ~exist('results', 'dir')
     mkdir('results')
 end
 
+color_veg = [70, 158, 53];
+color_not = [101, 67, 33];
+
 for extension = extensions
     all_images = dir(fullfile('./img/**/*.' + extension));
     
@@ -61,15 +64,26 @@ for extension = extensions
         matrix_exgi = 2*double(squeeze(handle(:,:,2))) - (double(squeeze(handle(:,:,1)))+double(squeeze(handle(:,:,3)))) - exgi_threshold; % EXGI 2*G-(R+B)
         matrix_boolean = min(1,max(0,matrix_exgi));
         
-        veg_cover = bwareaopen(matrix_boolean,dim_cluster);
-        veg_cover_handle = imshow(veg_cover, 'Border','tight');  
+        veg_cover_boolean = bwareaopen(matrix_boolean, dim_cluster);
 
-        processed_img_path = fullfile(img_dir, strcat(img_name(1:end-4), '_cover.', extension));
+        perc_list(i) = sum(sum(veg_cover_boolean))/(size(veg_cover_boolean,1)*size(veg_cover_boolean,2));
+        disp(perc_list)
+
+        veg_cover_colored(:,:,1) = color_not(1) + uint8(veg_cover_boolean * (color_veg(1) - color_not(1)));
+        veg_cover_colored(:,:,2) = color_not(2) + uint8(veg_cover_boolean * (color_veg(2) - color_not(2)));
+        veg_cover_colored(:,:,3) = color_not(3) + uint8(veg_cover_boolean * (color_veg(3) - color_not(3)));
+        
+        text_str = ['Cover: ' num2str(perc_list(i) * 100, '%0.2f') '%'];
+        position = [0 0];
+        box_color = {"black"};
+        veg_cover_colored= insertText(veg_cover_colored,position,text_str,FontSize=54,TextBoxColor=box_color, ...
+            BoxOpacity=0.4,TextColor="white");
+        veg_cover_handle = imshow(veg_cover_colored, 'Border','tight');
+
+        res_dir = img_dir;
+        processed_img_path = fullfile(res_dir, strcat(img_name(1:end-4), '_cover.', extension));
 
         saveas(veg_cover_handle, processed_img_path, lower(extension));
-
-        perc_list(i) = sum(sum(veg_cover))/(size(veg_cover,1)*size(veg_cover,2));
-        disp(perc_list)
         
         % figure;
         % imshow(handle, 'Border', 'tight');
